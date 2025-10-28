de48eed3137158d0eb1c3f1364d7380c5ed61400
Jason Zaelit <jzaelit1138@sdsu.edu>
2025-10-28 01:10:59 -0700

    initial implementation of pop() and upheap()

--- heap.h
+++ heap.h
@@ -15,23 +15,39 @@ struct MinHeap {
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
         data[size] = idx;
+        upheap(size, weightArr);
         size++;
-        upheap(idx, weightArr);
     }
 
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
         // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        int index = data[0];
+        data[0] = data[size - 1];
+        size--;
+        downheap(0, weightArr);
+        return (index);
     }
 
     void upheap(int pos, int weightArr[]) {
         // TODO: swap child upward while smaller than parent
-
+        while (pos > 0) {
+            int p = pos/2;
+            if (weightArr[data[pos]] < weightArr[data[p]]) {
+                int temp = data[pos];
+                data[pos] = data[p];
+                data[p] = temp;
+                pos = p;
+            }
+        }
     }
 
     void downheap(int pos, int weightArr[]) {
         // TODO: swap parent downward while larger than any child
+        int index = pos;
+        while (weightArr[index] > weightArr[pos/2]) {
+
+        }
     }
 };
 
