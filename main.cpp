06f7b06d4f8c24c614f48734cf413d325b4ce61c
Jason Zaelit <jzaelit1138@sdsu.edu>
2025-11-03 23:48:26 -0800

    implementation of generateCodes() and fully working implementation of main.cpp with documentation

--- main.cpp
+++ main.cpp
@@ -95,17 +95,20 @@ int buildEncodingTree(int nextFree) {
     //    - Set left/right pointers
     //    - Push new parent index back into the heap
     // 4. Return the index of the last remaining node (root)
-    MinHeap* heap = new MinHeap();
-    heap->push(nextFree, weightArr);
-    while (heap.size > 1) {
-        int first = heap -> pop(weightArr);
-        int second = heap -> pop(weightArr);
-        int parent = first + second;
-        heap -> push(parent, weightArr);
-        return (heap[heap.size]);
+    MinHeap heap; //created MinHeap object
+    for (int i = 0; i < nextFree; i++) {
+        heap.push(i, weightArr); //pushes all leaf node indices to heap
     }
-
-
+    while (heap.size > 1) { //while heap size greater than 1
+        int small1 = heap.pop(weightArr); //pops smallest node
+        int small2 = heap.pop(weightArr); //pops second smallest node
+        weightArr[nextFree] = weightArr[small1] + weightArr[small2]; //combines smallest node weights into a parent node
+        leftArr[nextFree] = small1; //assigns parent node left child to smallest node
+        rightArr[nextFree] = small2; //assigns parent node right child to second smallest node
+        heap.push(nextFree, weightArr); //pushes new parent index back into heap
+        nextFree++; //increments nextFree to make space for creation of new parent nodes
+    }
+    return (heap.pop(weightArr)); //returns the index value stored in the root node of the heap
 }
 
 // Step 4: Use an STL stack to generate codes
@@ -114,6 +117,28 @@ void generateCodes(int root, string codes[]) {
     // Use stack<pair<int, string>> to simulate DFS traversal.
     // Left edge adds '0', right edge adds '1'.
     // Record code when a leaf node is reached.
+    stack< pair<int, string> > stack; //created Stack object
+    stack.push(pair<int, string>(root, "")); //pushes root pair to stack
+    while (!stack.empty()) { //checks while stack still has nodes
+        pair<int, string> rootNode = stack.top(); //assigns a pair to the root node of stack
+        stack.pop(); //pops the root from stack
+
+        int index = rootNode.first; //stores the index of the root node
+        string code = rootNode.second; //initializes the code string associated with the node
+
+        if (leftArr[index] == -1 && rightArr[index] == -1) { //checks to see if the node is a leaf node
+            char c = charArr[index]; //if leaf node assigns corresponding char value to char variable
+            codes[c - 'a'] = code; //stores corresponding code in the order that the char is found in the alphabet
+        }
+        else {
+            if (rightArr[index] != -1) { //if the node is not a leaf node and has a right child
+                stack.push(pair<int,string>(rightArr[index], code + "1")); //pushes right child to stack and adds 1 to code for right edge traversal
+            }
+            if (leftArr[index] != -1) {
+                stack.push(pair<int,string>(leftArr[index], code + "0")); //pushes left child to stack and adds 0 to code for left edge traversal
+            }
+        }
+    }
 }
 
 // Step 5: Print table and encoded message
